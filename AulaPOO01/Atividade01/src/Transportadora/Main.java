790403b239bede0a9cd7105aa57d01a371b589d9
Efigênio Guerra Neto <efigenioguerra@hotmail.com>
2023-08-03 15:13:17 -0300

    POO

--- AulaPOO01/Atividade01/src/Transportadora/Main.java
+++ AulaPOO01/Atividade01/src/Transportadora/Main.java
@@ -2,8 +2,8 @@ package Transportadora;
 
 public class Main {
     public static void main(String[] args){
-        TransporteTerrestre transporteTerrestre = new TransporteTerrestre(10,10);
-        TransporteNautico transporteNautico = new TransporteNautico(10,10);
+        TransporteTerrestre transporteTerrestre = new TransporteTerrestre(20,10);
+        TransporteNautico transporteNautico = new TransporteNautico(20,10);
 
         System.out.println(transporteNautico.calculaFrete());
         System.out.println(transporteTerrestre.calculaFrete());
